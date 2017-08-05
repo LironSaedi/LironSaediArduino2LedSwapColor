5409c3a5376b5f9afe2c4a6f70135f77f97fdb93
LironSaedi <lionsaedi@gmail.com>
2017-08-04 17:28:22 -0700

    Finished led swap

--- LironSaediArduino2LedSwap.ino
+++ LironSaediArduino2LedSwap.ino
@@ -1,26 +1,39 @@
 int ledRed = 3;
 int ledGreen = 6;
-int fadeSpeed = 1;
+int fadeSpeed = 3;
 int brightness =  0;
-int button= 11;                         
-void setup() 
+int button = 12;
+bool pressed =false;
+bool previousButtonState;
+void setup()
 {
- pinMode(ledRed,OUTPUT);
- pinMode(ledGreen,OUTPUT);
+  pinMode(ledRed, OUTPUT);
+  pinMode(ledGreen, OUTPUT);
+  pinMode(button, INPUT_PULLUP);
+//  digitalWrite(ledGreen, HIGH);
+previousButtonState = false;
 }
 
 void loop()
 {
-  
+
   analogWrite(ledRed, brightness);
   brightness += fadeSpeed;
-   delay(10);
+  delay(10);
   if (brightness >= 255 ||  brightness <= 0  )
   {
-  fadeSpeed = -fadeSpeed;
- 
+    fadeSpeed = -fadeSpeed;
   }
-  //constantly add fadeSpeed to brightness
-  //if you reach 255 or 0, make fadeSpeed the negative version of itself
+  if (digitalRead(button) == LOW && previousButtonState == true)
+  {
+   pressed = !pressed;
+  }
+
+  if (pressed == true)
+  {
+    analogWrite(ledGreen, brightness);
+  }
+
+  previousButtonState = digitalRead(button);
 
 }
